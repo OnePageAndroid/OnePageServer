a5449c511642d303958e61e4471dbec4b5fff122
sumusb@naver.com <sumusb@naver.com>
2017-01-09 15:53:29 +0900

    개발환경 설정

---
+++ src/main/java/kr/nexters/OnepageApplication.java
@@ -0,0 +1,12 @@
+package kr.nexters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepageApplication.class, args);
+	}
+}
