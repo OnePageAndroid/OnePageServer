484fa4d8085ef8ab192516edc6fe17d57864ed29
seed(BoBinLee) <globaldev@coupang.com>
2017-01-09 22:33:02 +0900

    예외처리 추가 , config 설정.

--- src/main/java/kr/nexters/onepage/api/exception/ApiExceptionHandler.java
+++ src/main/java/kr/nexters/onepage/api/exception/ApiExceptionHandler.java
@@ -1,6 +1,8 @@
 package kr.nexters.onepage.api.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ApiExceptionHandler {
 	@ExceptionHandler({ Exception.class })
-	public String handleException(HttpServletRequest request, Exception exception) {
-		return "error";
+	public ResponseEntity<String> handleException(HttpServletRequest request, Exception exception) {
+		return new ResponseEntity("error", HttpStatus.BAD_REQUEST);
 	}
 }
